44ccb112bfdfee7cc4387fcb63d783f15e26e6b5
lukeworgan-design <lukeworgan@gmail.com>
2026-03-01 14:42:17 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 # bot
 import urllib.request
-urllib.request.urlretrieve('https://raw.githubusercontent.com/lukeworgan-design/Polar/main/real.py', 'r.py')
+urllib.request.urlretrieve('https://gist.githubusercontent.com/lukeworgan-design/cf3f7394a1144e7b35183644e46ed740/raw/real.py', 'r.py')
 exec(open('r.py').read())
